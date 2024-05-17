1a54686a3a377bc3922ff8e984b9c786263dd702
Anh Cao <anhcao4922@gmail.com>
2024-05-17 23:59:12 +0300

    setup: project setup and first service defined (example only)
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

---
+++ endpoints.ts
@@ -0,0 +1,8 @@
+// Define routes and corresponding microservices
+const services = [
+	{
+		route: "/stormbreaker",
+		target: "https://your-deployed-service.herokuapp.com/stormbreaker",
+	},
+	// Add more services as needed either deployed or locally.
+];
---
+++ index.ts
@@ -0,0 +1,13 @@
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// Create an instance of Express app
+const app = express();
+
+// Middleware setup
+app.use(cors()); // Enable CORS
+app.use(helmet()); // Add security headers
+app.use(morgan("combined")); // Log HTTP requests
+app.disable("x-powered-by"); // Hide Express server information
